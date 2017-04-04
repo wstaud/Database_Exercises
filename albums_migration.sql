b1b6eb500750b366b4cfae00f92be18fbbad61d4
xconstaud <xconstaud@gmail.com>
2017-04-04 10:33:40 -0500

    Update albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(100) NOT NULL,
-    content TEXT NOT NULL,
+    artist VARCHAR(50) NOT NULL DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL DEFAULT 'NONE', 
+    release_date YEAR(4),
+    sales DECIMAL(12, 2),
+    genre CHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '46.3', 'Pop, Rock, R&B'),
+       ('AC/DC', 'Back in Black', '1980', '26.1', 'Hard rock'),
+       ('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.2', 'Progressive rock'),
+       ('Whitney Houston/Various artists', 'The Bodyguard', '1992', '27.4', 'R&B, Soul, Pop, Soundtrack');
+
