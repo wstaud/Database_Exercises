a930b4d7749c5ec8643903d87f536b07d09d03b6
xconstaud <xconstaud@gmail.com>
2017-04-03 21:53:30 -0500

    Add default template

---
+++ albums_migration.sql
@@ -0,0 +1,7 @@
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
