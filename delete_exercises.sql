4a09e35ab942dd0369b5dbf180cca10620086223
xconstaud <xconstaud@gmail.com>
2017-04-04 15:52:13 -0500

    update delete exercise to delete instead of select

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,9 +1,9 @@
 USE codeup_test_db;
 
-SELECT * FROM albums WHERE release_date < 1991;
+DELETE FROM albums WHERE release_date < 1991;
 
-SELECT * FROM albums WHERE genre = '%disco%';
+DELETE FROM albums WHERE genre = '%disco%';
 
-SELECT * FROM albums WHERE artist = 'Michael Jackson';
+DELETE FROM albums WHERE artist = 'Michael Jackson';
 
 
