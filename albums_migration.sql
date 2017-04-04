348ccbbcb6b53b052c62a58ad0fe2044a88bfcda
xconstaud <xconstaud@gmail.com>
2017-04-04 14:03:05 -0500

    Add update db exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL DEFAULT 'NONE',
     name VARCHAR(100) NOT NULL DEFAULT 'NONE', 
-    release_date YEAR(4),
+    release_date INT,
     sales DECIMAL(12, 2),
     genre CHAR(50) NOT NULL,
     PRIMARY KEY (id)
---
+++ update_exercises.sql
@@ -0,0 +1,18 @@
+USE codeup_test_db;
+
+SELECT * FROM albums;
+UPDATE albums
+SET sales = sales / 2;
+SELECT * FROM albums;
+
+UPDATE albums
+SET release_date = 1800
+WHERE release_date < 1980;
+
+SELECT 'Michal Jackson stuff' AS 'NOTICE:';
+SELECT artist FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+SELECT artist FROM albums WHERE artist = 'Peter Jackson';
+
