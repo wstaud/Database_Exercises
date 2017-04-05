99aed9980ed53d5acd7a76fed23ababc74358fe9
xconstaud <xconstaud@gmail.com>
2017-04-05 12:25:30 -0500

    update order_by_exercies with final sort steps

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -24,7 +24,8 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' or last_name LIKE '%E';
 SELECT * FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E';
 
 SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25'
-AND hire_date LIKE '199%';
+AND hire_date LIKE '199%'
+ORDER BY birth_date, hire_date DESC;
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' and last_name NOT LIKE '%qu%';
 
