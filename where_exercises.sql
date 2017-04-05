de79a68bf4b4d77305a703c8c1a7acad07063ade
xconstaud <xconstaud@gmail.com>
2017-04-05 12:14:20 -0500

    update where_exercises to include final content

--- where_exercises.sql
+++ where_exercises.sql
@@ -15,4 +15,12 @@ SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
+SELECT * FROM employees WHERE last_name LIKE 'E%' or last_name LIKE '%E';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E';
+
+SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25'
+AND hire_date LIKE '199%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' and last_name NOT LIKE '%qu%';
 
