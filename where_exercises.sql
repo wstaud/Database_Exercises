e70a0f3395ef3d374174cee475080e03f2ea8e55
xconstaud <xconstaud@gmail.com>
2017-04-05 11:24:41 -0500

    update where to include male only search

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,9 +2,10 @@ USE employees;
 
 SELECT * 
 FROM employees 
-WHERE first_name = 'Irena' 
+WHERE gender = 'M'
+and (first_name = 'Irena' 
 or first_name = 'Vidya'
-or first_name = 'Maya';
+or first_name = 'Maya');
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
