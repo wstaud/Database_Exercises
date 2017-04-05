68ec0aa6abce8b34a295794c94d8ccbd3a577552
xconstaud <xconstaud@gmail.com>
2017-04-05 12:19:21 -0500

    add order_by_exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,27 @@
+USE employees;
+
+SELECT * 
+FROM employees 
+WHERE gender = 'M'
+and (first_name = 'Irena' 
+or first_name = 'Vidya'
+or first_name = 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees WHERE hire_date LIKE '199%';
+
+SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' or last_name LIKE '%E';
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' and last_name LIKE '%E';
+
+SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25'
+AND hire_date LIKE '199%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' and last_name NOT LIKE '%qu%';
+
