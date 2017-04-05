6b87248e5f250819ed52da6c3f2043bbe4146b62
xconstaud <xconstaud@gmail.com>
2017-04-05 15:20:34 -0500

    update limit_exercise to include remainder of tasks

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,4 +4,13 @@ FROM employees
 GROUP BY last_name DESC
 LIMIT 10;
 
+# born on christmas, hired in 90s... limit 5
+SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25'
+AND hire_date LIKE '199%'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
 
+SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25'
+AND hire_date LIKE '199%'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
