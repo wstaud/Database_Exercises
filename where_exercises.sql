b43609c7773b26d803c0daa5482ddffd85dc64db
xconstaud <xconstaud@gmail.com>
2017-04-05 11:23:51 -0500

    update where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,10 @@
 USE employees;
 
-
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * 
+FROM employees 
+WHERE first_name = 'Irena' 
+or first_name = 'Vidya'
+or first_name = 'Maya';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
