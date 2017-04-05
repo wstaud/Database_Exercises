a29481d11f23efca189cef35694c1197066e1a3c
xconstaud <xconstaud@gmail.com>
2017-04-05 11:17:28 -0500

    Add where exercise pt1

---
+++ where_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees WHERE hire_date LIKE '199%';
+
+SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+
